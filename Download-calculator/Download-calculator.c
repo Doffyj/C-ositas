0edb0de66989d81307c743ac6683c75b6f85b8a7
Wciv <sanchezeguiajon@gmail.com>
2022-06-22 23:56:12 +0200

    V3

--- Download-calculator/Download-calculator.c
+++ Download-calculator/Download-calculator.c
@@ -2,17 +2,22 @@
  * File: Download-calculator.c
  * Author: xAthaz <xathaz@protonmail.com>
  * Date: 22/06/2022 @ 22:53:48
- * Last Modified Date: 22/06/2022 @ 23:39:01
+ * Last Modified Date: 22/06/2022 @ 23:54:16
  * Last Modified By: xAthaz <xathaz@protonmail.com>
  */
 #include <stdio.h>
+#ifdef _WIN32
+#include <Windows.h> 	//Para sleep en windows
+#else
+#include <unistd.h> 	//Para UNIX basados
+#endif
 
 int	ft_download_calc(float i, float j){
     float x, y;
 
     x = ((i * 1000) / j) / 60;
     y = x / 60;
-    printf("La duracion estimada de la descarga es de %f minutos\n o %f horas.\n", x, y);
+    printf("La duracion estimada de la descarga es de %.1f minutos\nLo que equivale a %.1f horas.\n", x, y);
     return 0;
 }
 
@@ -29,5 +34,7 @@ int	main(){
 	printf("Quieres calcular otra cosa?// 1-Si 2-No\n");
 	scanf("%d", &k);
     }while(k==1);
+    printf("Echale un ojo al info.txt, todos los dias se aprende algo nuevo (?)");
+    Sleep(3000);
     return 0;
 }
