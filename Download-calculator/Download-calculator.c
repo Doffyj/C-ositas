618d775f4bee1c5eaa18ac69ad350ff8fba768b0
Wciv <sanchezeguiajon@gmail.com>
2022-06-22 23:09:07 +0200

    V1 de la calculadora

---
+++ Download-calculator/Download-calculator.c
@@ -0,0 +1,33 @@
+/**
+ * File: Download-calculator.c
+ * Author: xAthaz <xathaz@protonmail.com>
+ * Date: 22/06/2022 @ 22:53:48
+ * Last Modified Date: 22/06/2022 @ 23:07:45
+ * Last Modified By: xAthaz <xathaz@protonmail.com>
+ */
+#include <stdio.h>
+
+int	ft_download_calc(float i, float j){
+    float x, y;
+
+    x = (i * 1024) / j;
+    y = x / 60;
+    printf("La duracion estimada de la descarga es de %f minutos o %f horas.", x, y);
+    return 0;
+}
+
+int	main(){
+    float i, j; // pedir datos
+    int k;	// para el loop
+    printf("Bienvenido a la calculadora de descargas.\n");
+    do{
+	printf("Cual es el tamanyo del item que vas a descargar? (en GB)\n");
+	scanf("%f", &i);
+	printf("Cual es la velocidad de descarga? (en MB)\n");
+	scanf("%f", &j);
+	ft_download_calc(i, j);
+	printf("Quieres calcular otra cosa?// 1-Si 2-No\n");
+	scanf("%d", &k);
+    }while(k==1);
+    return 0;
+}
