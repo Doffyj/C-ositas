30f8e2c6b6796dba21981d488c9455b6c65b58bf
Wciv <sanchezeguiajon@gmail.com>
2022-07-02 02:00:56 +0200

    V2+norminette

---
+++ rush00/b/rush00.c
@@ -0,0 +1,54 @@
+void	ft_putchar(char c);
+
+void	ft_midln(int x)
+{
+	int	cont;
+
+	cont = 2;
+	ft_putchar(124);
+	if (x >= 2)
+	{
+		while (cont < x)
+		{
+			ft_putchar(32);
+			cont++;
+		}
+		ft_putchar(124);
+	}
+	ft_putchar(10);
+}
+
+void	ft_lastln(int x)
+{
+	int	cont;
+
+	cont = 2;
+	ft_putchar(111);
+	if (x >= 2)
+	{
+		while (cont < x)
+		{
+			ft_putchar(45);
+			cont++;
+		}
+		ft_putchar(111);
+	}
+	ft_putchar(10);
+}
+
+void	rush(int x, int y)
+{
+	int	i;
+
+	i = 2;
+	ft_lastln(x);
+	while (i < y)
+	{
+		ft_midln(x);
+		i++;
+	}
+	if (y > 1)
+	{
+		ft_lastln(x);
+	}
+}
