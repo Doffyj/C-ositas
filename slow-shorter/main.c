dced0607c722f9c143baec340b90e128cf53c725
Doffyj <sanchezeguiajon@gmail.com>
2022-08-18 07:08:06 +0200

    V1

---
+++ slow-shorter/main.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jonsanch <jonsanch@student.42urduliz.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/18 05:35:29 by jonsanch          #+#    #+#             */
+/*   Updated: 2022/08/18 06:20:51 by jonsanch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int	main(int argc, char **argv)
+{
+	int		i;
+	int		j;
+	int		temp;
+	char	arr[50];
+	int		args;
+
+	i = 0;
+	j = 1;
+	args = argc - 1;
+	printf("# of numbers to arrange = %d\n", args);
+	//arr = malloc((argc) * sizeof(char));
+	printf("Original string: ");
+	while (i < args)
+	{
+		printf("%c", argv[j][0]);
+		if (j < args)
+			printf(" ");
+		arr[i] = argv[j][0] - '0';
+		i++;
+		j++;
+	}
+	j = 1;
+	while (j <= i - 1)
+	{
+		if (arr[j - 1] > arr[j])
+		{
+			temp = arr[j];
+			arr[j] = arr[j - 1];
+			arr[j - 1] = temp;
+			j = 1;
+		}
+		j++;		
+	}
+	j = 0;
+	printf("\nResult: ");
+	while (j < args)
+	{
+		printf("%d", arr[j]);
+		if (j + 1 < args)
+			printf(" ");
+		j++;
+	}
+	printf("\n");
+	return (0);
+}
---
+++ slow-shorter/readme.txt
@@ -0,0 +1,10 @@
+La idea viene de un rearranger de arrays que nunca llegué a hacer.
+De momento es la V1 y sólo ordena números de dígito único.
+
+TODO:
+-Aceptar números más largos
+-Filtro para aceptar sólo argumentos numéricos
+-Poder aceptar números negativos
+-Tal vez aceptar números maś grandes que int? Tal vez hasta long
+
+Hasta entonces, encantado de hablar contigo amigo.
