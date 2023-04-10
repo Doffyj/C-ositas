15feb343fc8421d10f04f32eb3c3cbf593629eb9
jon <sanchezeguiajon@gmail.com>
2023-04-10 17:50:08 +0200

    Doomsday + leap year check

--- doomsday-algo/doomsdayalgo.c
+++ doomsday-algo/doomsdayalgo.c
@@ -1,12 +1,36 @@
 #include <stdio.h>
 
+int	ft_isleapy(int y)
+{
+	int	l2y = ((y / 100) * 100);
+	if (y % 4 == 0 && y != l2y)
+		return (1);
+	return (0);
+}
+
+int	ft_getmonthdd(int month, int year)
+{
+	if (month != 1 && month != 2)
+	{
+		if (month % 2 == 0)
+			return month;
+		else if (month > 6)
+			return (month - 4);
+		else
+			return (month + 4);
+	}
+	if (month == 1)
+		return (3 + ft_isleapy(year));
+	return (28 + ft_isleapy(year));
+}
+
 int	main(void)
 {
 	char	conwayday[7][20] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 	int inday, inmonth, iny;
 	printf("Introduce the date (dd/mm/yy):\n");
 	scanf("%d/%d/%d", &inday, &inmonth, &iny);
-	
+	int monthdd = ft_getmonthdd(inmonth, iny);
 	int	ancday = ((iny / 100) % 4);
 	switch(ancday)
 	{
